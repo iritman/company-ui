944271e3c0889f33c38f47884625b06439509cb0
Naiem YF <iritman@gmail.com>
2021-12-07 16:48:22 +0330

    Activate Antd styles in index.js and Predesign Login Page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+import "antd/dist/antd.css";
+import "./styles/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ConfigProvider } from "antd";
--- src/pages/login-page.jsx
+++ src/pages/login-page.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { Row, Col } from "antd";
+import Colors from "./../resources/colors";
 
 const LoginPage = () => {
-  return <h1>Login Page</h1>;
+  return (
+    <Row>
+      <Col
+        xs={24}
+        md={12}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          backgroundColor: Colors.blue[6],
+        }}
+      >
+        A
+      </Col>
+      <Col
+        xs={24}
+        md={12}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          backgroundColor: Colors.red[6],
+        }}
+      >
+        B
+      </Col>
+    </Row>
+  );
 };
 
 export default LoginPage;
