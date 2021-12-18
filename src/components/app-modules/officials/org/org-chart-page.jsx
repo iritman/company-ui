eef5816773dd15bb0c9186aa6d1c22a8f1d68fec
Naiem YF <iritman@gmail.com>
2021-12-18 13:21:55 +0330

    BugFix: Correct Org Chart Rendering After Page Load

--- src/components/app-modules/officials/org/org-chart-page.jsx
+++ src/components/app-modules/officials/org/org-chart-page.jsx
@@ -1,36 +1,29 @@
 import React, { useState } from "react";
 import { useMount } from "react-use";
-// import { Typography } from "antd";
 import service from "./../../../../services/org/departments-service";
-// import OrgTree from "react-org-tree";
-// import Colors from "./../../../../resources/colors";
 import { OrganizationGraph } from "@ant-design/charts";
-
-// const { Text } = Typography;
-
-// const config = {
-//   horizontal: false,
-//   collapsable: true,
-//   expandAll: true,
-//   labelClassName: "bg-white",
-// };
 
 const OrgChartPage = () => {
   const [departments, setDepartments] = useState([]);
-  const [nodes, setNodes] = useState(null);
 
   useMount(async () => {
     const data = await service.getAllData();
 
     setDepartments(data);
-
-    const node_list = getAntdNodes(
-      data.filter((n) => n.ParentDepartmentID === 0)[0],
-      departments
-    );
-
-    setNodes(node_list);
   });
+
+  const getNodes = (departments) => {
+    let node_list = {};
+
+    if (departments.length > 0) {
+      node_list = getAntdNodes(
+        departments.filter((n) => n.ParentDepartmentID === 0)[0],
+        departments
+      );
+    }
+
+    return node_list;
+  };
 
   const getAntdNodes = (department, depList) => {
     let children = [];
@@ -52,58 +45,15 @@ const OrgChartPage = () => {
     };
   };
 
-  //   const { horizontal, collapsable, expandAll, labelClassName } = config;
-
-  //   const getNodes = (department, depList) => {
-  //     let children = [];
-
-  //     const { DepartmentID, DepartmentTitle } = department;
-
-  //     const subDepartments = depList.filter(
-  //       (d) => d.ParentDepartmentID === DepartmentID
-  //     );
-
-  //     subDepartments.forEach((dep) => {
-  //       children = [...children, getNodes(dep, depList)];
-  //     });
-
-  //     return {
-  //       id: DepartmentID,
-  //       label: DepartmentTitle,
-  //       children,
-  //     };
-  //   };
-
   return (
     <>
-      {nodes !== null && (
+      {departments.length > 0 && (
         <OrganizationGraph
-          data={nodes}
+          data={getNodes(departments)}
           behaviors={["drag-canvas", "zoom-canvas", "drag-node"]}
         />
       )}
     </>
-    // <div
-    //   className="m-t-lg text-center scrolling-wrapper-flexbox"
-    //   style={{ direction: "ltr" }}
-    // >
-    //   {nodes !== null && (
-    //     <OrgTree
-    //       data={nodes}
-    //       horizontal={horizontal}
-    //       collapsable={collapsable}
-    //       labelClassName={labelClassName}
-    //       expandAll={expandAll}
-    //       renderContent={(data) => {
-    //         return <Text style={{ color: Colors.blue[7] }}>{data.label}</Text>;
-    //       }}
-    //       onClick={(e, data) => {
-    //         //todo
-    //         console.log(data);
-    //       }}
-    //     ></OrgTree>
-    //   )}
-    // </div>
   );
 };
 
