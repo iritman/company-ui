777232e26c25cb23e1d2eb273be65c51db7b14d4
Naiem YF <iritman@gmail.com>
2021-12-25 18:27:54 +0330

    CLEAN: Remove Unused 'useState' From Modal Modules

--- src/components/app-modules/officials/org/duty-modal.jsx
+++ src/components/app-modules/officials/org/duty-modal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useMount } from "react-use";
 import { Form, Row, Col } from "antd";
 import Joi from "joi-browser";
--- src/components/app-modules/officials/org/member-modal.jsx
+++ src/components/app-modules/officials/org/member-modal.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useMount } from "react-use";
 import { Form, Row, Col, message, Button, Avatar, Space, Tooltip } from "antd";
 import {
