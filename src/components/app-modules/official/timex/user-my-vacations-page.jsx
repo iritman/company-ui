e87f94adfaf3961e279d39c070ae5d3bf2fa0247
Naiem YF <iritman@gmail.com>
2022-03-28 13:48:40 +0430

    Refactor vacations pages and simplify codes!

--- src/components/app-modules/official/timex/user-my-vacations-page.jsx
+++ src/components/app-modules/official/timex/user-my-vacations-page.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { useMount } from "react-use";
-import { Spin, Row, Col, Typography, Button } from "antd";
+import { Spin, Row, Col, Button } from "antd";
 import { InfoCircleOutlined as InfoIcon } from "@ant-design/icons";
 import Words from "../../../../resources/words";
-import utils from "../../../../tools/utils";
 import service from "../../../../services/official/timex/user-my-vacations-service";
 import {
-  getSorter,
   checkAccess,
   getColumns,
   GetSimplaDataPageMethods,
@@ -18,187 +16,7 @@ import Colors from "../../../../resources/colors";
 import VacationModal from "./user-my-vacation-modal";
 import SearchModal from "./user-my-vacation-search-modal";
 import DetailsModal from "./vacation-details-modal";
-
-const { Text } = Typography;
-
-const getVacationStatusColor = (statusID) => {
-  let color = Colors.grey[6];
-
-  switch (statusID) {
-    case 2:
-      color = Colors.green[6];
-      break;
-    case 3:
-      color = Colors.red[6];
-      break;
-    default:
-      color = Colors.grey[6];
-      break;
-  }
-
-  return color;
-};
-
-const getVacationStatusTitle = (statusID) => {
-  let title = Words.in_progress;
-
-  switch (statusID) {
-    case 2:
-      title = Words.accepted;
-      break;
-    case 3:
-      title = Words.rejected;
-      break;
-    default:
-      title = Words.in_progress;
-      break;
-  }
-
-  return title;
-};
-
-const getSheets = (records) => [
-  {
-    title: "MyVacations",
-    data: records,
-    columns: [
-      { label: Words.id, value: "VacationID" },
-      {
-        label: Words.full_name,
-        value: (record) => `${record.FirstName} ${record.LastName}`,
-      },
-      {
-        label: Words.vacation_type,
-        value: (record) => `${record.VacationTypeTitle}`,
-      },
-      {
-        label: Words.reg_date,
-        value: (record) => utils.slashDate(record.RegDate),
-      },
-      {
-        label: Words.reg_time,
-        value: (record) => utils.colonTime(record.RegTime),
-      },
-      {
-        label: Words.from_date,
-        value: (record) => utils.slashDate(record.StartDate),
-      },
-      {
-        label: Words.from_time,
-        value: (record) => utils.colonTime(record.StartTime),
-      },
-      {
-        label: Words.to_date,
-        value: (record) => utils.slashDate(record.FinishDate),
-      },
-      {
-        label: Words.to_time,
-        value: (record) => utils.colonTime(record.FinishTime),
-      },
-      {
-        label: Words.descriptions,
-        value: (record) => record.DetailsText,
-      },
-      {
-        label: Words.status,
-        value: (record) => getVacationStatusTitle(record.FinalStatusID),
-      },
-    ],
-  },
-];
-
-const baseColumns = [
-  {
-    title: Words.id,
-    width: 75,
-    align: "center",
-    dataIndex: "VacationID",
-    sorter: getSorter("VacationID"),
-    render: (VacationID) => <Text>{utils.farsiNum(`${VacationID}`)}</Text>,
-  },
-  {
-    title: Words.vacation_type,
-    width: 120,
-    align: "center",
-    dataIndex: "VacationTypeTitle",
-    sorter: getSorter("VacationTypeTitle"),
-    render: (VacationTypeTitle) => (
-      <Text style={{ color: Colors.blue[6] }}>{VacationTypeTitle}</Text>
-    ),
-  },
-  {
-    title: Words.from_date,
-    width: 120,
-    align: "center",
-    dataIndex: "StartDate",
-    sorter: getSorter("StartDate"),
-    render: (StartDate) => (
-      <Text style={{ color: Colors.green[6] }}>
-        {`${utils.weekDayNameFromText(StartDate)} - ${utils.farsiNum(
-          utils.slashDate(StartDate)
-        )}`}
-      </Text>
-    ),
-  },
-  {
-    title: Words.from_time,
-    width: 100,
-    align: "center",
-    dataIndex: "StartTime",
-    sorter: getSorter("StartTime"),
-    render: (StartTime) => (
-      <>
-        {StartTime.Length > 0 && (
-          <Text style={{ color: Colors.magenta[6] }}>
-            {`${utils.farsiNum(utils.colonTime(StartTime))}`}
-          </Text>
-        )}
-      </>
-    ),
-  },
-  {
-    title: Words.to_date,
-    width: 120,
-    align: "center",
-    dataIndex: "FinishDate",
-    sorter: getSorter("FinishDate"),
-    render: (FinishDate) => (
-      <Text style={{ color: Colors.green[6] }}>
-        {`${utils.weekDayNameFromText(FinishDate)} - ${utils.farsiNum(
-          utils.slashDate(FinishDate)
-        )}`}
-      </Text>
-    ),
-  },
-  {
-    title: Words.to_time,
-    width: 100,
-    align: "center",
-    dataIndex: "FinishTime",
-    sorter: getSorter("FinishTime"),
-    render: (FinishTime) => (
-      <>
-        {FinishTime.Length > 0 && (
-          <Text style={{ color: Colors.magenta[6] }}>
-            {`${utils.farsiNum(utils.colonTime(FinishTime))}`}
-          </Text>
-        )}
-      </>
-    ),
-  },
-  {
-    title: Words.status,
-    width: 100,
-    align: "center",
-    dataIndex: "FinalStatusID",
-    sorter: getSorter("FinalStatusID"),
-    render: (FinalStatusID) => (
-      <Text style={{ color: getVacationStatusColor(FinalStatusID) }}>
-        {getVacationStatusTitle(FinalStatusID)}
-      </Text>
-    ),
-  },
-];
+import { getSheets, baseColumns } from "./vacations-page-items";
 
 const handleCheckEditable = (row) => row.Editable;
 const handleCheckDeletable = (row) => row.Deletable;
@@ -286,7 +104,7 @@ const UserMyVacationsPage = ({ pageName }) => {
         <Row gutter={[10, 15]}>
           <SimpleDataPageHeader
             title={Words.my_vacations}
-            sheets={getSheets(records)}
+            sheets={getSheets(records, "MyVacations")}
             fileName="MyVacations"
             onSearch={() => setShowSearchModal(true)}
             onClear={handleClear}
