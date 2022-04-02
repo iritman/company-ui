003b5d5f29146b004946ed1a9128b34e7ae56ff0
Naiem YF <iritman@gmail.com>
2022-04-02 21:51:10 +0430

    Refactor mission details module to support security person view

--- src/components/common/mission-details.jsx
+++ src/components/common/mission-details.jsx
@@ -12,7 +12,7 @@ import MemberProfileImage from "./member-profile-image";
 const { Text } = Typography;
 const { Step } = Steps;
 
-const MissionDetails = ({ mission }) => {
+const MissionDetails = ({ mission, securityPersonView }) => {
   const [currentStep, setCurrentStep] = useState(0);
 
   const handleStepChange = (current) => {
@@ -207,100 +207,104 @@ const MissionDetails = ({ mission }) => {
     },
   ];
 
-  Actions.forEach((action) => {
-    steps.push({
-      stepID: action.StepID,
-      title: action.SysRoleTitle,
-      status: action.MemberID > 0 ? "finish" : "wait",
-      content: (
-        <>
-          {action.MemberID === 0 ? (
-            <Alert
-              message={
-                <Text>
-                  {Words.messages.no_response_submitted.replace(
-                    "#",
-                    action.SysRoleTitle
-                  )}
-                </Text>
-              }
-              type="warning"
-              showIcon
-            />
-          ) : (
-            <Descriptions
-              bordered
-              column={{
-                //   md: 2, sm: 2,
-                lg: 2,
-                md: 2,
-                xs: 1,
-              }}
-              size="middle"
-            >
-              <Descriptions.Item
-                label={`${Words.response} ${action.SysRoleTitle}`}
-              >
-                <Space>
-                  {action.IsAccepted ? (
-                    <CheckIcon
-                      style={{ color: Colors.green[7], fontSize: 18 }}
-                    />
-                  ) : (
-                    <CloseIcon style={{ color: Colors.red[7], fontSize: 18 }} />
-                  )}
-                  <Text
-                    style={{
-                      color: action.IsAccepted
-                        ? Colors.green[6]
-                        : Colors.red[6],
-                    }}
-                  >
-                    {action.IsAccepted ? Words.accepted : Words.rejected}
+  if (!securityPersonView) {
+    Actions.forEach((action) => {
+      steps.push({
+        stepID: action.StepID,
+        title: action.SysRoleTitle,
+        status: action.MemberID > 0 ? "finish" : "wait",
+        content: (
+          <>
+            {action.MemberID === 0 ? (
+              <Alert
+                message={
+                  <Text>
+                    {Words.messages.no_response_submitted.replace(
+                      "#",
+                      action.SysRoleTitle
+                    )}
                   </Text>
-                </Space>
-              </Descriptions.Item>
-              <Descriptions.Item label={Words.reg_member}>
-                <Text
-                  style={{
-                    color: Colors.orange[7],
-                  }}
+                }
+                type="warning"
+                showIcon
+              />
+            ) : (
+              <Descriptions
+                bordered
+                column={{
+                  //   md: 2, sm: 2,
+                  lg: 2,
+                  md: 2,
+                  xs: 1,
+                }}
+                size="middle"
+              >
+                <Descriptions.Item
+                  label={`${Words.response} ${action.SysRoleTitle}`}
                 >
-                  {`${action.FirstName} ${action.LastName}`}
-                </Text>
-              </Descriptions.Item>
-              {action.DetailsText.length > 0 && (
-                <Descriptions.Item label={Words.descriptions} span={2}>
+                  <Space>
+                    {action.IsAccepted ? (
+                      <CheckIcon
+                        style={{ color: Colors.green[7], fontSize: 18 }}
+                      />
+                    ) : (
+                      <CloseIcon
+                        style={{ color: Colors.red[7], fontSize: 18 }}
+                      />
+                    )}
+                    <Text
+                      style={{
+                        color: action.IsAccepted
+                          ? Colors.green[6]
+                          : Colors.red[6],
+                      }}
+                    >
+                      {action.IsAccepted ? Words.accepted : Words.rejected}
+                    </Text>
+                  </Space>
+                </Descriptions.Item>
+                <Descriptions.Item label={Words.reg_member}>
                   <Text
                     style={{
-                      color: Colors.purple[7],
-                      whiteSpace: "pre-line",
+                      color: Colors.orange[7],
                     }}
                   >
-                    {utils.farsiNum(action.DetailsText)}
+                    {`${action.FirstName} ${action.LastName}`}
                   </Text>
                 </Descriptions.Item>
-              )}
-              <Descriptions.Item label={Words.reg_date}>
-                <Text style={{ color: valueColor }}>
-                  {utils.farsiNum(
-                    `${utils.weekDayNameFromText(
-                      action.ActionDate
-                    )} ${utils.slashDate(action.ActionDate)}`
-                  )}
-                </Text>
-              </Descriptions.Item>
-              <Descriptions.Item label={Words.reg_time}>
-                <Text style={{ color: valueColor }}>
-                  {utils.farsiNum(`${utils.colonTime(action.ActionTime)}`)}
-                </Text>
-              </Descriptions.Item>
-            </Descriptions>
-          )}
-        </>
-      ),
+                {action.DetailsText.length > 0 && (
+                  <Descriptions.Item label={Words.descriptions} span={2}>
+                    <Text
+                      style={{
+                        color: Colors.purple[7],
+                        whiteSpace: "pre-line",
+                      }}
+                    >
+                      {utils.farsiNum(action.DetailsText)}
+                    </Text>
+                  </Descriptions.Item>
+                )}
+                <Descriptions.Item label={Words.reg_date}>
+                  <Text style={{ color: valueColor }}>
+                    {utils.farsiNum(
+                      `${utils.weekDayNameFromText(
+                        action.ActionDate
+                      )} ${utils.slashDate(action.ActionDate)}`
+                    )}
+                  </Text>
+                </Descriptions.Item>
+                <Descriptions.Item label={Words.reg_time}>
+                  <Text style={{ color: valueColor }}>
+                    {utils.farsiNum(`${utils.colonTime(action.ActionTime)}`)}
+                  </Text>
+                </Descriptions.Item>
+              </Descriptions>
+            )}
+          </>
+        ),
+      });
     });
-  });
+  }
 
   if (NeedVehicle && OfficialIsVehicleApproved) {
     steps = [
