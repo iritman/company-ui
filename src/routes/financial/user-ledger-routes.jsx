0bd5a01e6ae2778d2a8f0953f67a04fb2124efa3
Naiem YF <iritman@gmail.com>
2022-10-04 23:28:12 +0330

    Implement ledger doc types page & modals

--- src/routes/financial/user-ledger-routes.jsx
+++ src/routes/financial/user-ledger-routes.jsx
@@ -5,7 +5,7 @@ import ProtectedRoute from "../../components/common/protected-route";
 import LedgerDashboard from "../../components/app-modules/financial/ledger/ledger-dashboard";
 import FinancialPeriodsPage from "../../components/app-modules/financial/ledger/financial-periods-page";
 // import LedgersPage from "../../components/app-modules/financial/ledger/ledgers-page";
-// import DocTypesPage from "../../components/app-modules/financial/ledger/doc-types-page";
+import DocTypesPage from "../../components/app-modules/financial/ledger/doc-types-page";
 //---
 
 const modulePath = "financial/ledger";
@@ -27,12 +27,12 @@ const UserPublicSettingsRoutes = ({ path }) => {
         path={`${path}/${modulePath}/ledgers`}
         exact
         render={() => <LedgersPage pageName="Ledgers" />}
-      />
+      />*/}
       <ProtectedRoute
         path={`${path}/${modulePath}/doc-types`}
         exact
         render={() => <DocTypesPage pageName="DocTypes" />}
-      /> */}
+      />
       <Redirect to="/not-found" />
     </Switch>
   );
