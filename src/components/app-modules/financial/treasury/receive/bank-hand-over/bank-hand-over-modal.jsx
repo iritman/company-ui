31f2fd950da4cee9dd241f771b8adfdb74cfc6e7
Naiem YF <iritman@gmail.com>
2022-12-25 21:00:34 +0330

    Refactor bank hand over modal and separate it's code

--- src/components/app-modules/financial/treasury/receive/bank-hand-over/bank-hand-over-modal.jsx
+++ src/components/app-modules/financial/treasury/receive/bank-hand-over/bank-hand-over-modal.jsx
@@ -1,22 +1,7 @@
 import React, { useState } from "react";
 import { useMount } from "react-use";
-import {
-  Form,
-  Row,
-  Col,
-  Typography,
-  Space,
-  Popconfirm,
-  Button,
-  Tabs,
-} from "antd";
-import {
-  PlusOutlined as AddIcon,
-  DeleteOutlined as DeleteIcon,
-  EditOutlined as EditIcon,
-  QuestionCircleOutlined as QuestionIcon,
-} from "@ant-design/icons";
-import Joi from "joi-browser";
+import { Form, Row, Col, Space, Popconfirm, Button, Tabs } from "antd";
+import { QuestionCircleOutlined as QuestionIcon } from "@ant-design/icons";
 import ModalWindow from "../../../../../common/modal-window";
 import Words from "../../../../../../resources/words";
 import Colors from "../../../../../../resources/colors";
@@ -27,7 +12,6 @@ import {
   initModal,
   saveModalChanges,
   handleError,
-  getSorter,
 } from "../../../../../../tools/form-manager";
 import service from "../../../../../../services/financial/treasury/receive/bank-hand-overs-service";
 import DateItem from "../../../../../form-controls/date-item";
@@ -42,338 +26,16 @@ import ChequeModal from "./bank-hand-over-cheque-modal";
 import DemandModal from "./bank-hand-over-demand-modal";
 import { v4 as uuid } from "uuid";
 import PriceViewer from "../../../../../common/price-viewer";
+import {
+  schema,
+  initRecord,
+  getChequeColumns,
+  getDemandColumns,
+  getNewButton,
+  calculatePrice,
+} from "./bank-hand-over-modal-code";
 
-const { Text } = Typography;
 const { TabPane } = Tabs;
-
-const schema = {
-  HandOverID: Joi.number().required().label(Words.id),
-  CompanyBankAccountID: Joi.number()
-    .min(1)
-    .required()
-    .label(Words.bank_account),
-  CurrencyID: Joi.number().min(1).required().label(Words.currency),
-  ItemType: Joi.number().min(1).required().label(Words.item_type),
-  HandOverDate: Joi.string().required().label(Words.hand_over_date),
-  OperationID: Joi.number().label(Words.financial_operation),
-  StandardDetailsID: Joi.number().label(Words.standard_description),
-  StatusID: Joi.number(),
-  Cheques: Joi.array(),
-  Demands: Joi.array(),
-};
-
-const initRecord = {
-  HandOverID: 0,
-  CompanyBankAccountID: 0,
-  CurrencyID: 0,
-  ItemType: 0,
-  HandOverDate: "",
-  OperationID: 0,
-  StandardDetailsID: 0,
-  StatusID: 1,
-  Cheques: [],
-  Demands: [],
-};
-
-const getChequeColumns = (access, statusID, onEdit, onDelete) => {
-  let columns = [
-    {
-      title: Words.id,
-      width: 75,
-      align: "center",
-      dataIndex: "ChequeID",
-      sorter: getSorter("ChequeID"),
-      render: (ChequeID) => (
-        <Text>{ChequeID > 0 ? utils.farsiNum(`${ChequeID}`) : ""}</Text>
-      ),
-    },
-    {
-      title: Words.front_side,
-      width: 200,
-      align: "center",
-      // dataIndex: "Title",
-      sorter: getSorter("LastName"),
-      render: (record) => (
-        <Text style={{ color: Colors.cyan[6] }}>
-          {utils.farsiNum(
-            record.FrontSideMemberID > 0
-              ? `${record.FrontSideFirstName} ${record.FrontSideLastName}`
-              : `${record.CompanyTitle}`
-          )}
-        </Text>
-      ),
-    },
-    {
-      title: Words.duration,
-      width: 100,
-      align: "center",
-      dataIndex: "DurationTypeTitle",
-      sorter: getSorter("DurationTypeTitle"),
-      render: (DurationTypeTitle) => (
-        <Text style={{ color: Colors.grey[6] }}>{DurationTypeTitle}</Text>
-      ),
-    },
-    {
-      title: Words.account_no,
-      width: 100,
-      align: "center",
-      dataIndex: "AccountNo",
-      sorter: getSorter("AccountNo"),
-      render: (AccountNo) => (
-        <Text style={{ color: Colors.orange[6] }}>
-          {utils.farsiNum(AccountNo)}
-        </Text>
-      ),
-    },
-    {
-      title: Words.bank,
-      width: 100,
-      align: "center",
-      dataIndex: "BankTitle",
-      sorter: getSorter("BankTitle"),
-      render: (BankTitle) => (
-        <Text style={{ color: Colors.green[6] }}>{BankTitle}</Text>
-      ),
-    },
-    {
-      title: Words.city,
-      width: 120,
-      align: "center",
-      dataIndex: "CityTitle",
-      sorter: getSorter("CityTitle"),
-      render: (CityTitle) => (
-        <Text style={{ color: Colors.blue[6] }}>{CityTitle}</Text>
-      ),
-    },
-    {
-      title: Words.bank_branch,
-      width: 100,
-      align: "center",
-      dataIndex: "BranchName",
-      sorter: getSorter("BranchName"),
-      render: (BranchName) => (
-        <Text style={{ color: Colors.grey[6] }}>
-          {utils.farsiNum(BranchName)}
-        </Text>
-      ),
-    },
-    {
-      title: Words.cheque_no,
-      width: 150,
-      align: "center",
-      dataIndex: "ChequeNo",
-      sorter: getSorter("ChequeNo"),
-      render: (ChequeNo) => (
-        <Text style={{ color: Colors.red[6] }}>{utils.farsiNum(ChequeNo)}</Text>
-      ),
-    },
-    {
-      title: Words.price,
-      width: 200,
-      align: "center",
-      dataIndex: "Amount",
-      sorter: getSorter("Amount"),
-      render: (Amount) => (
-        <Text style={{ color: Colors.green[6] }}>
-          {utils.farsiNum(utils.moneyNumber(Amount))}
-        </Text>
-      ),
-    },
-    {
-      title: Words.due_date,
-      width: 120,
-      align: "center",
-      dataIndex: "DueDate",
-      sorter: getSorter("DueDate"),
-      render: (DueDate) => (
-        <Text
-          style={{
-            color: Colors.geekblue[6],
-          }}
-        >
-          {utils.farsiNum(utils.slashDate(DueDate))}
-        </Text>
-      ),
-    },
-    {
-      title: Words.agreed_date,
-      width: 120,
-      align: "center",
-      dataIndex: "AgreedDate",
-      sorter: getSorter("AgreedDate"),
-      render: (AgreedDate) => (
-        <Text
-          style={{
-            color: Colors.blue[6],
-          }}
-        >
-          {utils.farsiNum(utils.slashDate(AgreedDate))}
-        </Text>
-      ),
-    },
-  ];
-
-  // StatusID : 1 => Not Approve, Not Reject! Just Save...
-  if (
-    statusID === 1 &&
-    ((access.CanDelete && onDelete) || (access.CanEdit && onEdit))
-  ) {
-    columns = [
-      ...columns,
-      {
-        title: "",
-        fixed: "right",
-        align: "center",
-        width: 75,
-        render: (record) => (
-          <Space>
-            {access.CanDelete && onDelete && (
-              <Popconfirm
-                title={Words.questions.sure_to_delete_selected_item}
-                onConfirm={async () => await onDelete(record)}
-                okText={Words.yes}
-                cancelText={Words.no}
-                icon={<QuestionIcon style={{ color: "red" }} />}
-              >
-                <Button type="link" icon={<DeleteIcon />} danger />
-              </Popconfirm>
-            )}
-
-            {access.CanEdit && onEdit && (
-              <Button
-                type="link"
-                icon={<EditIcon />}
-                onClick={() => onEdit(record)}
-              />
-            )}
-          </Space>
-        ),
-      },
-    ];
-  }
-
-  return columns;
-};
-
-const getDemandColumns = (access, statusID, onEdit, onDelete) => {
-  let columns = [
-    {
-      title: Words.id,
-      width: 75,
-      align: "center",
-      dataIndex: "DemandID",
-      sorter: getSorter("DemandID"),
-      render: (DemandID) => (
-        <Text>{DemandID > 0 ? utils.farsiNum(`${DemandID}`) : ""}</Text>
-      ),
-    },
-    {
-      title: Words.front_side,
-      width: 200,
-      align: "center",
-      // dataIndex: "Title",
-      sorter: getSorter("LastName"),
-      render: (record) => (
-        <Text style={{ color: Colors.cyan[6] }}>
-          {utils.farsiNum(
-            record.FrontSideMemberID > 0
-              ? `${record.FrontSideFirstName} ${record.FrontSideLastName}`
-              : `${record.CompanyTitle}`
-          )}
-        </Text>
-      ),
-    },
-    {
-      title: Words.duration,
-      width: 100,
-      align: "center",
-      dataIndex: "DurationTypeTitle",
-      sorter: getSorter("DurationTypeTitle"),
-      render: (DurationTypeTitle) => (
-        <Text style={{ color: Colors.grey[6] }}>{DurationTypeTitle}</Text>
-      ),
-    },
-    {
-      title: Words.demand_no,
-      width: 150,
-      align: "center",
-      dataIndex: "DemandNo",
-      sorter: getSorter("DemandNo"),
-      render: (DemandNo) => (
-        <Text style={{ color: Colors.red[6] }}>{utils.farsiNum(DemandNo)}</Text>
-      ),
-    },
-    {
-      title: Words.price,
-      width: 200,
-      align: "center",
-      dataIndex: "Amount",
-      sorter: getSorter("Amount"),
-      render: (Amount) => (
-        <Text style={{ color: Colors.green[6] }}>
-          {utils.farsiNum(utils.moneyNumber(Amount))}
-        </Text>
-      ),
-    },
-    {
-      title: Words.due_date,
-      width: 120,
-      align: "center",
-      dataIndex: "DueDate",
-      sorter: getSorter("DueDate"),
-      render: (DueDate) => (
-        <Text
-          style={{
-            color: Colors.geekblue[6],
-          }}
-        >
-          {utils.farsiNum(utils.slashDate(DueDate))}
-        </Text>
-      ),
-    },
-  ];
-
-  // StatusID : 1 => Not Approve, Not Reject! Just Save...
-  if (
-    statusID === 1 &&
-    ((access.CanDelete && onDelete) || (access.CanEdit && onEdit))
-  ) {
-    columns = [
-      ...columns,
-      {
-        title: "",
-        fixed: "right",
-        align: "center",
-        width: 75,
-        render: (record) => (
-          <Space>
-            {access.CanDelete && onDelete && (
-              <Popconfirm
-                title={Words.questions.sure_to_delete_selected_item}
-                onConfirm={async () => await onDelete(record)}
-                okText={Words.yes}
-                cancelText={Words.no}
-                icon={<QuestionIcon style={{ color: "red" }} />}
-              >
-                <Button type="link" icon={<DeleteIcon />} danger />
-              </Popconfirm>
-            )}
-
-            {access.CanEdit && onEdit && (
-              <Button
-                type="link"
-                icon={<EditIcon />}
-                onClick={() => onEdit(record)}
-              />
-            )}
-          </Space>
-        ),
-      },
-    ];
-  }
-
-  return columns;
-};
 
 const formRef = React.createRef();
 
@@ -466,6 +128,10 @@ const BankHandOverModal = ({
       setStandardDetails(StandardDetails);
       setHasSaveApproveAccess(HasSaveApproveAccess);
       setHasRejectAccess(HasRejectAccess);
+
+      if (selectedObject !== null) {
+        setSelectedTab(selectedObject.ItemType === 1 ? "cheques" : "demands");
+      }
     } catch (ex) {
       handleError(ex);
     }
@@ -683,19 +349,9 @@ const BankHandOverModal = ({
     }
   };
 
-  const getNewButton = () => {
-    return (
-      <Button
-        type="primary"
-        onClick={() => {
-          setSelectedItem(null);
-          handleShowNewModal();
-        }}
-        icon={<AddIcon />}
-      >
-        {Words.new}
-      </Button>
-    );
+  const handleNewButtonClick = () => {
+    setSelectedItem(null);
+    handleShowNewModal();
   };
 
   const getFooterButtons = (is_disable) => {
@@ -786,34 +442,11 @@ const BankHandOverModal = ({
     );
   };
 
-  //------
-
-  const calculatePrice = () => {
-    const price = {};
-    let sum = 0;
-
-    record.Cheques?.forEach((i) => {
-      sum += i.Amount;
-    });
-    price.ChequesAmount = sum;
-    sum = 0;
-
-    record.Demands?.forEach((i) => {
-      sum += i.Amount;
-    });
-    price.DemandsAmount = sum;
-    sum = 0;
-
-    for (const key in price) {
-      sum += price[key];
-    }
-    price.Total = sum;
-
-    return price;
-  };
-
-  const handleTabChange = (key) => {
-    setSelectedTab(key);
+  const handleChangeItemType = (value) => {
+    const rec = { ...record };
+    rec.ItemType = value || 0;
+    setRecord(rec);
+    setSelectedTab(value === 1 ? "cheques" : "demands");
   };
 
   //------
@@ -829,7 +462,7 @@ const BankHandOverModal = ({
   const status_id =
     selectedObject === null ? record.StatusID : selectedObject.StatusID;
 
-  const price = calculatePrice();
+  const price = calculatePrice(record);
 
   return (
     <>
@@ -863,6 +496,10 @@ const BankHandOverModal = ({
                 valueColumn="Title"
                 formConfig={formConfig}
                 required
+                disabled={
+                  record?.Cheques?.length > 0 || record?.Demands?.length > 0
+                }
+                onChange={handleChangeItemType}
               />
             </Col>
             <Col xs={24} md={12}>
@@ -916,57 +553,66 @@ const BankHandOverModal = ({
               </Col>
             )}
 
-            <Col xs={24}>
-              <Form.Item>
-                <Tabs
-                  type="card"
-                  defaultActiveKey="1"
-                  onChange={handleTabChange}
-                >
-                  <TabPane tab={Words.cheque} key="cheques">
-                    <Row gutter={[0, 15]}>
-                      <Col xs={24}>
-                        <DetailsTable
-                          records={record.Cheques}
-                          columns={getChequeColumns(
-                            access,
-                            status_id,
-                            handleEditCheque,
-                            handleDeleteCheque
-                          )}
-                        />
-                      </Col>
-                      <Col xs={24}>
-                        <PriceViewer price={price.ChequesAmount} />
-                      </Col>
-                    </Row>
-                  </TabPane>
-                  <TabPane tab={Words.demand} key="demands">
-                    <Row gutter={[0, 15]}>
-                      <Col xs={24}>
-                        <DetailsTable
-                          records={record.Demands}
-                          columns={getDemandColumns(
-                            access,
-                            status_id,
-                            handleEditDemand,
-                            handleDeleteDemand
-                          )}
-                        />
-                      </Col>
-                      <Col xs={24}>
-                        <PriceViewer price={price.DemandsAmount} />
-                      </Col>
-                    </Row>
-                  </TabPane>
-                </Tabs>
-              </Form.Item>
-            </Col>
+            {record?.ItemType > 0 && (
+              <>
+                <Col xs={24}>
+                  <Form.Item>
+                    <Tabs
+                      type="card"
+                      defaultActiveKey="0"
+                      // onChange={handleTabChange}
+                    >
+                      {record.ItemType === 1 && (
+                        <TabPane tab={Words.cheque} key="cheques">
+                          <Row gutter={[0, 15]}>
+                            <Col xs={24}>
+                              <DetailsTable
+                                records={record.Cheques}
+                                columns={getChequeColumns(
+                                  access,
+                                  status_id,
+                                  handleEditCheque,
+                                  handleDeleteCheque
+                                )}
+                              />
+                            </Col>
+                            <Col xs={24}>
+                              <PriceViewer price={price.ChequesAmount} />
+                            </Col>
+                          </Row>
+                        </TabPane>
+                      )}
 
-            {status_id === 1 && (
-              <Col xs={24}>
-                <Form.Item>{getNewButton()}</Form.Item>
-              </Col>
+                      {record.ItemType === 2 && (
+                        <TabPane tab={Words.demand} key="demands">
+                          <Row gutter={[0, 15]}>
+                            <Col xs={24}>
+                              <DetailsTable
+                                records={record.Demands}
+                                columns={getDemandColumns(
+                                  access,
+                                  status_id,
+                                  handleEditDemand,
+                                  handleDeleteDemand
+                                )}
+                              />
+                            </Col>
+                            <Col xs={24}>
+                              <PriceViewer price={price.DemandsAmount} />
+                            </Col>
+                          </Row>
+                        </TabPane>
+                      )}
+                    </Tabs>
+                  </Form.Item>
+                </Col>
+
+                {status_id === 1 && (
+                  <Col xs={24}>
+                    <Form.Item>{getNewButton(handleNewButtonClick)}</Form.Item>
+                  </Col>
+                )}
+              </>
             )}
           </Row>
         </Form>
