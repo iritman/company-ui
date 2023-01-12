460b4597eda280dcdefbb36fe738f8e978c8ff75
Naiem YF <iritman@gmail.com>
2023-01-12 12:22:17 +0330

    Handle Tafsil info in bank modal & bank details modal

--- src/components/app-modules/financial/treasury/basic-info/bank-details-modal.jsx
+++ src/components/app-modules/financial/treasury/basic-info/bank-details-modal.jsx
@@ -1,5 +1,14 @@
 import React from "react";
-import { Button, Modal, Row, Col, Typography, Descriptions, Alert } from "antd";
+import {
+  Button,
+  Modal,
+  Row,
+  Col,
+  Typography,
+  Descriptions,
+  Alert,
+  Tabs,
+} from "antd";
 import Words from "../../../../../resources/words";
 import Colors from "../../../../../resources/colors";
 import utils from "../../../../../tools/utils";
@@ -16,7 +25,100 @@ const BankDetailsModal = ({ selectedObject, isOpen, onOk }) => {
     Website,
     SwiftCode,
     DetailsText,
+    TafsilInfo,
   } = selectedObject;
+
+  const items = [
+    {
+      label: Words.info,
+      key: "info",
+      children: (
+        <Descriptions
+          bordered
+          column={{
+            //   md: 2, sm: 2,
+            lg: 2,
+            md: 2,
+            xs: 1,
+          }}
+          size="middle"
+        >
+          <Descriptions.Item label={Words.id}>
+            <Text style={{ color: Colors.red[6] }}>
+              {utils.farsiNum(`${BankID}`)}
+            </Text>
+          </Descriptions.Item>
+          <Descriptions.Item label={Words.bank_type}>
+            <Text style={{ color: Colors.cyan[6] }}>{BankTypeTitle}</Text>
+          </Descriptions.Item>
+          <Descriptions.Item label={Words.pr_tel_no}>
+            <Text style={{ color: valueColor }}>{PRTelNo}</Text>
+          </Descriptions.Item>
+          <Descriptions.Item label={Words.swift_code}>
+            <Text style={{ color: valueColor }}>{SwiftCode}</Text>
+          </Descriptions.Item>
+          <Descriptions.Item label={Words.website} span={2}>
+            <Text style={{ color: valueColor }}>{Website}</Text>
+          </Descriptions.Item>
+          {DetailsText.length > 0 && (
+            <Descriptions.Item label={Words.descriptions} span={2}>
+              <Text
+                style={{
+                  color: Colors.purple[7],
+                  whiteSpace: "pre-line",
+                }}
+              >
+                {utils.farsiNum(DetailsText)}
+              </Text>
+            </Descriptions.Item>
+          )}
+        </Descriptions>
+      ),
+    },
+    {
+      label: Words.tafsil_account,
+      key: "tafsil-account",
+      children: (
+        <>
+          {TafsilInfo === null ? (
+            <></>
+          ) : (
+            <Descriptions
+              bordered
+              column={{
+                //   md: 2, sm: 2,
+                lg: 2,
+                md: 2,
+                xs: 1,
+              }}
+              size="middle"
+            >
+              <Descriptions.Item label={Words.tafsil_id}>
+                <Text style={{ color: Colors.red[6] }}>
+                  {utils.farsiNum(`${TafsilInfo.TafsilAccountID}`)}
+                </Text>
+              </Descriptions.Item>
+              <Descriptions.Item label={Words.tafsil_code}>
+                <Text style={{ color: Colors.cyan[6] }}>
+                  {utils.farsiNum(TafsilInfo.TafsilCode)}
+                </Text>
+              </Descriptions.Item>
+              <Descriptions.Item label={Words.tafsil_type}>
+                <Text style={{ color: valueColor }}>
+                  {TafsilInfo.TafsilTypeTitle}
+                </Text>
+              </Descriptions.Item>
+              <Descriptions.Item label={Words.title}>
+                <Text style={{ color: valueColor }}>
+                  {utils.farsiNum(TafsilInfo.TafsilAccountTitle)}
+                </Text>
+              </Descriptions.Item>
+            </Descriptions>
+          )}
+        </>
+      ),
+    },
+  ];
 
   return (
     <Modal
@@ -48,46 +150,7 @@ const BankDetailsModal = ({ selectedObject, isOpen, onOk }) => {
               />
             </Col>
             <Col xs={24}>
-              <Descriptions
-                bordered
-                column={{
-                  //   md: 2, sm: 2,
-                  lg: 2,
-                  md: 2,
-                  xs: 1,
-                }}
-                size="middle"
-              >
-                <Descriptions.Item label={Words.id}>
-                  <Text style={{ color: Colors.red[6] }}>
-                    {utils.farsiNum(`${BankID}`)}
-                  </Text>
-                </Descriptions.Item>
-                <Descriptions.Item label={Words.bank_type}>
-                  <Text style={{ color: Colors.cyan[6] }}>{BankTypeTitle}</Text>
-                </Descriptions.Item>
-                <Descriptions.Item label={Words.pr_tel_no}>
-                  <Text style={{ color: valueColor }}>{PRTelNo}</Text>
-                </Descriptions.Item>
-                <Descriptions.Item label={Words.swift_code}>
-                  <Text style={{ color: valueColor }}>{SwiftCode}</Text>
-                </Descriptions.Item>
-                <Descriptions.Item label={Words.website} span={2}>
-                  <Text style={{ color: valueColor }}>{Website}</Text>
-                </Descriptions.Item>
-                {DetailsText.length > 0 && (
-                  <Descriptions.Item label={Words.descriptions} span={2}>
-                    <Text
-                      style={{
-                        color: Colors.purple[7],
-                        whiteSpace: "pre-line",
-                      }}
-                    >
-                      {utils.farsiNum(DetailsText)}
-                    </Text>
-                  </Descriptions.Item>
-                )}
-              </Descriptions>
+              <Tabs defaultActiveKey="1" type="card" items={items} />
             </Col>
           </Row>
         </article>
--- src/components/app-modules/financial/treasury/basic-info/bank-modal.jsx
+++ src/components/app-modules/financial/treasury/basic-info/bank-modal.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { useMount } from "react-use";
-import { Form, Row, Col } from "antd";
+import { Form, Row, Col, Tabs, Typography, Descriptions } from "antd";
 import Joi from "joi-browser";
 import ModalWindow from "../../../../common/modal-window";
 import Words from "../../../../../resources/words";
+import Colors from "../../../../../resources/colors";
 import utils from "../../../../../tools/utils";
 import {
   validateForm,
@@ -19,6 +20,9 @@ import {
   useModalContext,
   useResetContext,
 } from "../../../../contexts/modal-context";
+
+const { Text } = Typography;
+const valueColor = Colors.blue[7];
 
 const schema = {
   BankID: Joi.number().required(),
@@ -133,18 +137,11 @@ const BankModal = ({ isOpen, selectedObject, onOk, onCancel }) => {
 
   //------
 
-  return (
-    <ModalWindow
-      isOpen={isOpen}
-      isEdit={isEdit}
-      inProgress={progress}
-      disabled={validateForm({ record, schema }) && true}
-      onClear={clearRecord}
-      onSubmit={handleSubmit}
-      onCancel={onCancel}
-      width={750}
-    >
-      <Form ref={formRef} name="dataForm">
+  let items = [
+    {
+      label: Words.info,
+      key: "info",
+      children: (
         <Row gutter={[5, 1]} style={{ marginLeft: 1 }}>
           <Col xs={24}>
             <InputItem
@@ -202,6 +199,74 @@ const BankModal = ({ isOpen, selectedObject, onOk, onCancel }) => {
             />
           </Col>
         </Row>
+      ),
+    },
+  ];
+
+  if (selectedObject !== null) {
+    const { TafsilInfo } = selectedObject;
+
+    items = [
+      ...items,
+      {
+        label: Words.tafsil_account,
+        key: "tafsil-account",
+        children: (
+          <>
+            {TafsilInfo === null ? (
+              <></>
+            ) : (
+              <Descriptions
+                bordered
+                column={{
+                  //   md: 2, sm: 2,
+                  lg: 2,
+                  md: 2,
+                  xs: 1,
+                }}
+                size="middle"
+              >
+                <Descriptions.Item label={Words.tafsil_id}>
+                  <Text style={{ color: Colors.red[6] }}>
+                    {utils.farsiNum(`${TafsilInfo.TafsilAccountID}`)}
+                  </Text>
+                </Descriptions.Item>
+                <Descriptions.Item label={Words.tafsil_code}>
+                  <Text style={{ color: Colors.cyan[6] }}>
+                    {utils.farsiNum(TafsilInfo.TafsilCode)}
+                  </Text>
+                </Descriptions.Item>
+                <Descriptions.Item label={Words.tafsil_type}>
+                  <Text style={{ color: valueColor }}>
+                    {TafsilInfo.TafsilTypeTitle}
+                  </Text>
+                </Descriptions.Item>
+                <Descriptions.Item label={Words.title}>
+                  <Text style={{ color: valueColor }}>
+                    {utils.farsiNum(TafsilInfo.TafsilAccountTitle)}
+                  </Text>
+                </Descriptions.Item>
+              </Descriptions>
+            )}
+          </>
+        ),
+      },
+    ];
+  }
+
+  return (
+    <ModalWindow
+      isOpen={isOpen}
+      isEdit={isEdit}
+      inProgress={progress}
+      disabled={validateForm({ record, schema }) && true}
+      onClear={clearRecord}
+      onSubmit={handleSubmit}
+      onCancel={onCancel}
+      width={750}
+    >
+      <Form ref={formRef} name="dataForm">
+        <Tabs defaultActiveKey="1" type="card" items={items} />
       </Form>
     </ModalWindow>
   );
