1acf1014d9714e2beec83aa7e6ef904ae98ef35d
Naiem YF <iritman@gmail.com>
2023-07-27 16:27:57 +0330

    Check whether records is empty or not in the WorkTimeStatistics module

--- src/components/common/work-time-statistics.jsx
+++ src/components/common/work-time-statistics.jsx
@@ -7,30 +7,40 @@ import Words from "../../resources/words";
 const WorkTimeStatistics = ({ type, data }) => {
   return (
     <>
-      <Col xs={24}>
-        <ValueLabel
-          title={
-            type === "mission" ? Words.work_time_daily : Words.vacation_daily
-          }
-          value={`${utils.getWorkTimeInfo(data).days} ${Words.day}`}
-        />
-      </Col>
-      <Col xs={24}>
-        <ValueLabel
-          title={
-            type === "mission" ? Words.work_time_timely : Words.vacation_timely
-          }
-          value={utils.minToTime(utils.getWorkTimeInfo(data).remain_mins)}
-        />
-      </Col>
-      <Col xs={24}>
-        <ValueLabel
-          title={
-            type === "mission" ? Words.work_time_total : Words.vacation_total
-          }
-          value={utils.workTimeToText(utils.getWorkTimeInfo(data))}
-        />
-      </Col>
+      {data.length > 0 && (
+        <>
+          <Col xs={24}>
+            <ValueLabel
+              title={
+                type === "mission"
+                  ? Words.work_time_daily
+                  : Words.vacation_daily
+              }
+              value={`${utils.getWorkTimeInfo(data).days} ${Words.day}`}
+            />
+          </Col>
+          <Col xs={24}>
+            <ValueLabel
+              title={
+                type === "mission"
+                  ? Words.work_time_timely
+                  : Words.vacation_timely
+              }
+              value={utils.minToTime(utils.getWorkTimeInfo(data).remain_mins)}
+            />
+          </Col>
+          <Col xs={24}>
+            <ValueLabel
+              title={
+                type === "mission"
+                  ? Words.work_time_total
+                  : Words.vacation_total
+              }
+              value={utils.workTimeToText(utils.getWorkTimeInfo(data))}
+            />
+          </Col>
+        </>
+      )}
     </>
   );
 };
